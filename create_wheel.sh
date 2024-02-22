fe03dc05ce63a74a47af2f60b492646bfaf155a6
Kalle Minkner <info@kalleminkner.de>
2024-02-22 22:41:06 +0100

    delete old dists before creating new ones

--- create_wheel.sh
+++ create_wheel.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+# install wheel if not done already
 pip install wheel
+# delete old dist packages if any
+rm -rf ./dist/*
+# build new packages if any
 python setup.py bdist_wheel --universal
