3d21b3b88ec27e1a34c217aae13a883266d4d58a
Kalle Minkner <info@kalleminkner.de>
2024-02-22 22:32:09 +0100

    add long description for pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
-from setuptools import setup, find_packages
+from pathlib import Path
 from idotmatrix.version import __version__
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="idotmatrix",
     version=__version__,
     description="configure any iDotMatrix compatible 16x16 or 32x32 pixel display without the chinese iDotMatrix android / iOS app.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/derkalle4/python3-idotmatrix-library",
     author="Kalle Minkner",
     author_email="info@kalleminkner.de",
